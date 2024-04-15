81cd1799e3a93bac4670568e8811a3e6580da2e4
YunZi.Song <yoonji5809@gmail.com>
2024-04-15 14:07:51 +0900

    Fix: 홈스크린, 뷰모델 작성중

--- lib/presentation/home/home_screen.dart
+++ lib/presentation/home/home_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:pair_coding/presentation/home/home_view_model.dart';
 
 class HomeScreen extends StatefulWidget {
   const HomeScreen({super.key});
@@ -10,6 +11,34 @@ class HomeScreen extends StatefulWidget {
 class _HomeScreenState extends State<HomeScreen> {
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    final viewModel = HomeViewModel();
+    return Scaffold(
+      body: Column(
+        children: [
+          Row(
+            children: [
+              Text('시간'),
+              Text('도착지'),
+              Text('열차 종류'),
+              Text('열차 번호'),
+            ],
+          ),
+          Expanded(
+            child: ListView.builder(
+              itemCount: 1,
+              itemBuilder: (context, index) {
+                return Row(children: [
+                  // TODO. 뷰모델 데이터 연결
+                  Text('시간'),
+                  Text('도착지'),
+                  Text('열차 종류'),
+                  Text('열차 번호'),
+                ]);
+              },
+            ),
+          ),
+        ],
+      ),
+    );
   }
 }
---
+++ lib/presentation/home/home_view_model.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/cupertino.dart';
+
+import '../../data/model/subway_model.dart';
+
+class HomeViewModel with ChangeNotifier{
+
+  final List<SubwayModel> subways =[];
+
+}
