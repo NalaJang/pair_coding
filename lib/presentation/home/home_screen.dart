118a85473f6d452ee8bca9be75bd4b214724ec46
YunZi.Song <yoonji5809@gmail.com>
2024-04-15 16:03:38 +0900

    Feat: UI 디자인 수정

--- lib/presentation/home/home_screen.dart
+++ lib/presentation/home/home_screen.dart
@@ -1,4 +1,7 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
+import 'package:intl/intl.dart';
 import 'package:pair_coding/presentation/home/home_view_model.dart';
 import 'package:provider/provider.dart';
 
@@ -21,103 +24,141 @@ class _HomeScreenState extends State<HomeScreen> {
   @override
   Widget build(BuildContext context) {
     final viewModel = context.watch<HomeViewModel>();
+    final currentTime = DateFormat("hh시 mm분").format(DateTime.now());
 
     return Scaffold(
       appBar: AppBar(
         centerTitle: true,
         title: const Text('출발'),
       ),
-      body: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: [
-          const SizedBox(
-            height: 60,
-          ),
-          Row(
+      body: SafeArea(
+        child: Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 10),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
             children: [
+              const SizedBox(
+                height: 60,
+              ),
               Row(
                 children: [
-                  Text('현재 시간'),
-                  Text('${DateTime.now().toLocal()}'),
+                  Row(
+                    children: [
+                      const Text('현재 시간'),
+                      const SizedBox(width: 10),
+                      Text(currentTime),
+                    ],
+                  ),
                 ],
               ),
+              const SizedBox(height: 20),
+              const Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Expanded(
+                    child: Text(
+                      '위치',
+                      textAlign: TextAlign.center,
+                    ),
+                  ),
+                  Expanded(
+                    child: Text(
+                      '도착지',
+                      textAlign: TextAlign.center,
+                    ),
+                  ),
+                  Expanded(
+                    child: Text(
+                      '열차 종류',
+                      textAlign: TextAlign.center,
+                    ),
+                  ),
+                  Expanded(
+                    child: Text('열차 번호'),
+                  ),
+                ],
+              ),
+              Expanded(
+                  child: ListView(
+                children: [
+                  Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: viewModel.subways
+                        .map((e) => SizedBox(
+                              height: 50,
+                              child: Row(
+                                mainAxisAlignment:
+                                    MainAxisAlignment.spaceBetween,
+                                children: [
+                                  // 위치
+                                  Expanded(
+                                    flex: 1,
+                                    child: Text(
+                                      e.arrvalMessage,
+                                      textAlign: TextAlign.center,
+                                      overflow: TextOverflow.ellipsis,
+                                      style: TextStyle(
+                                        color: viewModel.subways[0] == e
+                                            ? Colors.red
+                                            : Colors.black,
+                                      ),
+                                    ),
+                                  ),
+
+                                  // 도착지
+                                  Expanded(
+                                    flex: 2,
+                                    child: Text(
+                                      e.trainLineNm,
+                                      overflow: TextOverflow.fade,
+                                      textAlign: TextAlign.center,
+                                      style: TextStyle(
+                                        color: viewModel.subways[0] == e
+                                            ? Colors.red
+                                            : Colors.black,
+                                      ),
+                                    ),
+                                  ),
+
+                                  // 열차 종류
+                                  Expanded(
+                                    flex: 1,
+                                    child: Text(
+                                      textAlign: TextAlign.center,
+                                      e.btrainSttus,
+                                      overflow: TextOverflow.ellipsis,
+                                      style: TextStyle(
+                                        color: viewModel.subways[0] == e
+                                            ? Colors.red
+                                            : Colors.black,
+                                      ),
+                                    ),
+                                  ),
+
+                                  // 열차 번호
+                                  Expanded(
+                                    flex: 1,
+                                    // width: 50,
+                                    child: Text(
+                                      '${e.btrainNo}',
+                                      overflow: TextOverflow.ellipsis,
+                                      style: TextStyle(
+                                        color: viewModel.subways[0] == e
+                                            ? Colors.red
+                                            : Colors.black,
+                                      ),
+                                    ),
+                                  ),
+                                ],
+                              ),
+                            ))
+                        .toList(),
+                  )
+                ],
+              )),
             ],
           ),
-          const Row(
-            mainAxisAlignment: MainAxisAlignment.spaceBetween,
-            children: [
-              Text('위치'),
-              Text('도착지'),
-              Text('열차 종류'),
-              Text('열차 번호'),
-            ],
-          ),
-          Expanded(
-            child: ListView(
-              children: [
-                Column(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: viewModel.subways
-                      .map((e) => Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                    children: [
-                      // 위치
-                      SizedBox(
-                        // width: 100,
-                        child: Text(
-                          e.arrvalMessage,
-                          overflow: TextOverflow.ellipsis,
-                          style: TextStyle(
-                            color:
-                            viewModel.subways[0] == e ? Colors.red : Colors.black,
-                          ),
-                        ),
-                      ),
-
-                      // 도착지
-                      SizedBox(
-                        child: Text(
-                          e.trainLineNm,
-                          overflow: TextOverflow.ellipsis,
-                          style: TextStyle(
-                            color:
-                            viewModel.subways[0] == e ? Colors.red : Colors.black,
-                          ),
-                        ),
-                      ),
-
-                      // 열차 종류
-                      SizedBox(
-                        child: Text(
-                          e.btrainSttus,
-                          overflow: TextOverflow.ellipsis,
-                          style: TextStyle(
-                            color:
-                            viewModel.subways[0] == e ? Colors.red : Colors.black,
-                          ),
-                        ),
-                      ),
-
-                      // 열차 번호
-                      SizedBox(
-                        // width: 50,
-                        child: Text(
-                          '${e.btrainNo}',
-                          overflow: TextOverflow.ellipsis,
-                          style: TextStyle(
-                            color:
-                            viewModel.subways[0] == e ? Colors.red : Colors.black,
-                          ),
-                        ),
-                      ),
-                    ],
-                  ))
-                      .toList(),
-                )
-              ],
-            )
-          ),
-        ],
+        ),
       ),
     );
   }
