14aee663558887270db21ab26932a5c67dc8a76d
nala0897@naver.com <nala0897@naver.com>
2024-04-16 16:11:02 +0900

    feat: dropdownButton 구현 완료

--- lib/presentation/rate/rate_screen.dart
+++ lib/presentation/rate/rate_screen.dart
@@ -22,89 +22,89 @@ class _RateScreenState extends State<RateScreen> {
   @override
   Widget build(BuildContext context) {
     final viewModel = context.watch<RateViewModel>();
-    String dropdownValue = viewModel.rate.conversionRates.first.country;
 
     return Scaffold(
       appBar: AppBar(
         title: const Text('환율'),
       ),
-      body: Column(
-        children: [
-          Row(
-            children: [
-              //TODO.토글
-              Flexible(
-                // flex: 1,
-                child: TextFormField(
-                  initialValue: '달러',
-                  textInputAction: TextInputAction.done,
-                  keyboardType: TextInputType.number,
-                  decoration: const InputDecoration(
-                    hintText: '가격 입력',
-                    border: InputBorder.none,
-                  ),
-                  textAlign: TextAlign.center,
-                  style: const TextStyle(
-                    fontSize: 13,
-                  ),
+      body: viewModel.dropdownValue.isEmpty
+          ? const Center(
+              child: CircularProgressIndicator(),
+            )
+          : Column(
+              children: [
+                Row(
+                  children: [
+                    Flexible(
+                      // flex: 1,
+                      child: TextFormField(
+                        controller: viewModel.inputController,
+                        initialValue: '${viewModel.rate.conversionRates.first.rate}',
+                        textInputAction: TextInputAction.done,
+                        keyboardType: TextInputType.number,
+                        decoration: const InputDecoration(
+                          hintText: '가격 입력',
+                          border: InputBorder.none,
+                        ),
+                        textAlign: TextAlign.center,
+                        style: const TextStyle(
+                          fontSize: 13,
+                        ),
+                      ),
+                    ),
+
+                    DropdownButton(
+                      value: viewModel.dropdownValue,
+                      items: viewModel.rate.conversionRates
+                          .map((e) => DropdownMenuItem<String>(
+                                value: e.country,
+                                child: Text(e.country),
+                              ))
+                          .toList(),
+                      onChanged: (value) {
+                        viewModel.changeCountry(value!);
+                      },
+                    ),
+                  ],
                 ),
-              ),
 
-              DropdownButton(
-                value: dropdownValue,
-                items: viewModel.rate.conversionRates
-                    .map((e) => DropdownMenuItem<String>(
-                          value: e.country,
-                          child: Text(e.country),
-                        ))
-                    .toList(),
-                onChanged: (value) {
-                  setState(() {
-                    dropdownValue = value!;
-                  });
-                },
+                Row(
+                  children: [
+                    Flexible(
+                      child: TextFormField(
+                        controller: viewModel.outputController,
+                        initialValue: '${viewModel.rate.conversionRates.first.rate}',
+                        textInputAction: TextInputAction.done,
+                        keyboardType: TextInputType.number,
+                        decoration: const InputDecoration(
+                          hintText: '가격 입력',
+                          border: InputBorder.none,
+                        ),
+                        textAlign: TextAlign.center,
+                        style: const TextStyle(
+                          fontSize: 13,
+                        ),
+                      ),
+                    ),
 
-                // viewModel.rate.conversionRates.map<DropdownMenuItem<String>>((String value) {
-                //   return DropdownMenuItem<String> (
-                //     value: value,
-                //     child: Text(value),
-                //   );
-                // }).toList(),
-                // onChanged: (value) {
-                //   setState(() {
-                //     dropdownValue = value!;
-                //   });
-                // },
-              ),
-            ],
-          ),
-          Flexible(
-            // flex: 1,
-            child: TextFormField(
-              initialValue: '',
-              textInputAction: TextInputAction.done,
-              keyboardType: TextInputType.number,
-              decoration: const InputDecoration(
-                hintText: '국가 선택',
-                border: InputBorder.none,
-              ),
-              textAlign: TextAlign.center,
-              style: const TextStyle(
-                fontSize: 13,
-              ),
+                    DropdownButton(
+                      value: viewModel.dropdownValue,
+                      items: viewModel.rate.conversionRates
+                          .map((e) => DropdownMenuItem<String>(
+                        value: e.country,
+                        child: Text(e.country),
+                      ))
+                          .toList(),
+                      onChanged: (value) {
+                        viewModel.changeCountry(value!);
+                      },
+                    ),
+                  ],
+                ),
+
+                Text(viewModel.rate.timeLastUpdateUtc),
+              ],
             ),
-          ),
-
-          Text(viewModel.rate.timeLastUpdateUtc),
-          // Expanded(
-          //   child: ListView(
-          //     children: viewModel.rate.conversionRates
-          //         .map((e) => Text('${e.country} : ${e.rate}'))
-          //         .toList(),
-          //   ),
-          // )
-        ],
-      ),
     );
   }
 }
--- lib/presentation/rate/rate_view_model.dart
+++ lib/presentation/rate/rate_view_model.dart
@@ -4,20 +4,39 @@ import 'package:pair_coding/data/repository/rate_repository.dart';
 
 class RateViewModel with ChangeNotifier {
   final RateRepository repository;
+  String _dropdownValue = '';
   Rate _rate = const Rate(timeLastUpdateUtc: '', conversionRates: []);
+  final _inputController = TextEditingController();
+  final _outputController = TextEditingController();
 
   RateViewModel({
     required this.repository,
   });
 
+  @override
+  void dispose() {
+    _inputController.dispose();
+    _outputController.dispose();
+    super.dispose();
+  }
+
   Rate get rate => _rate;
+
+  String get dropdownValue => _dropdownValue;
+
+  get inputController => _inputController;
+
+  get outputController => _outputController;
 
   void getRate() async {
     _rate = await repository.getRate();
-    print(_rate.conversionRates.length);
+    _dropdownValue = _rate.conversionRates.first.country;
 
     notifyListeners();
   }
 
-  void selectedCountry() async {}
+  void changeCountry(String value) {
+    _dropdownValue = value;
+    notifyListeners();
+  }
 }
