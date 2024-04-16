79318996e57c775cebafb71304e82d298d2dbb7d
nala0897@naver.com <nala0897@naver.com>
2024-04-16 15:29:53 +0900

    feat: dropdownButton 수정 중

--- lib/presentation/rate/rate_screen.dart
+++ lib/presentation/rate/rate_screen.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
+import 'package:pair_coding/data/model/conversion_rate.dart';
 import 'package:pair_coding/presentation/rate/rate_view_model.dart';
 import 'package:provider/provider.dart';
 
@@ -21,6 +22,7 @@ class _RateScreenState extends State<RateScreen> {
   @override
   Widget build(BuildContext context) {
     final viewModel = context.watch<RateViewModel>();
+    String? dropdownValue = viewModel.rate.conversionRates.first.country;
 
     return Scaffold(
       appBar: AppBar(
@@ -32,7 +34,7 @@ class _RateScreenState extends State<RateScreen> {
             children: [
               //TODO.토글
               Flexible(
-                flex: 1,
+                // flex: 1,
                 child: TextFormField(
                   initialValue: '달러',
                   textInputAction: TextInputAction.done,
@@ -47,16 +49,32 @@ class _RateScreenState extends State<RateScreen> {
                   ),
                 ),
               ),
+
+              DropdownButton(
+                value: dropdownValue,
+                // items: viewModel.rate.conversionRates.map((e) => e.country).toList(),
+                items: viewModel.rate.conversionRates.map<DropdownMenuItem<ConversionRate>>((ConversionRate value) {
+                  return DropdownMenuItem<ConversionRate> (
+                    value: value,
+                    child: Text(value.country),
+                  );
+                }).toList(),
+                onChanged: (value) {
+                  setState(() {
+                    dropdownValue = value!;
+                  });
+                },
+              ),
             ],
           ),
           Flexible(
-            flex: 1,
+            // flex: 1,
             child: TextFormField(
               initialValue: '',
               textInputAction: TextInputAction.done,
               keyboardType: TextInputType.number,
               decoration: const InputDecoration(
-                hintText: '가격 입력',
+                hintText: '국가 선택',
                 border: InputBorder.none,
               ),
               textAlign: TextAlign.center,
