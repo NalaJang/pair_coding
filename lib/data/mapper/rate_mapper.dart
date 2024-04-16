946d32db0d983cf7d54540e1c156c800e0b8b257
YunZi.Song <yoonji5809@gmail.com>
2024-04-16 15:13:33 +0900

    Fix: rate mapper 수정

--- lib/data/mapper/rate_mapper.dart
+++ lib/data/mapper/rate_mapper.dart
@@ -1,11 +1,21 @@
 import 'package:pair_coding/data/dto/rate_dto.dart';
 import 'package:pair_coding/data/model/rate_model.dart';
 
+import '../model/conversion_rate.dart';
+
 extension RateMapper on RateDto {
   Rate toRate() {
+
+    final newConversionRates = conversionRates?.entries
+        .map((entry) => ConversionRate(
+              country: entry.key,
+              rate: entry.value,
+            ))
+        .toList();
+
     return Rate(
       timeLastUpdateUtc: timeLastUpdateUtc ?? '',
-      conversionRates: conversionRates ?? [],
+      conversionRates: newConversionRates ?? [],
     );
   }
 }
